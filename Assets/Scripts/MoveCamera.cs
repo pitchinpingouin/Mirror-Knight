7e2aa3978adfe63b07b2940cacd391cafaadc5e8
pitchinpingouin <35998664+pitchinpingouin@users.noreply.github.com>
2019-09-25 16:28:56 +0200

    gestion de la caméra smooth

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     private GameObject player;
-    private Transform cameraTarget;
+    private Transform cameraTargetTransform;
+    private Vector3 newPosition;
+    [SerializeField] float timeDurationTravelling;
+    private float timer = 0.0f;
+
+    private Vector3 currentVelocity;
 
     void Start()
     {
+        currentVelocity = Vector3.zero;
         player = GameObject.FindGameObjectWithTag("Player");
-        cameraTarget = player.transform.Find("cameraTarget");
+        cameraTargetTransform = player.transform.Find("cameraTarget");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //cameraTarget = player.transform.Find("cameraTarget").position;
-        transform.position = cameraTarget.position;
+        transform.position = Vector3.SmoothDamp(transform.position, cameraTargetTransform.position, ref currentVelocity, timeDurationTravelling);
     }
 }
---
+++ Assets/Scripts/MoveCameraTargetAccordingToMousePosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraTargetAccordingToMousePosition : MonoBehaviour
+{
+    private InputManager inputManager;
+    private Transform cameraTargetTransform;
+    [SerializeField] private float divisionFactorToPlaceTargetBetweenPlayerAndMouse;
+    [SerializeField] private float maxDistanceBetweenPlayerAndMouseToMoveTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = GetComponent<InputManager>();
+        cameraTargetTransform = transform.Find("cameraTarget");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, inputManager.lookAtTarget) <= maxDistanceBetweenPlayerAndMouseToMoveTarget)
+        {
+            cameraTargetTransform.localPosition = new Vector3(cameraTargetTransform.localPosition.x, cameraTargetTransform.localPosition.y, Vector3.Distance(transform.position, inputManager.lookAtTarget) / divisionFactorToPlaceTargetBetweenPlayerAndMouse);
+        }
+        else
+        {
+            cameraTargetTransform.localPosition = new Vector3(cameraTargetTransform.localPosition.x, cameraTargetTransform.localPosition.y, maxDistanceBetweenPlayerAndMouseToMoveTarget / divisionFactorToPlaceTargetBetweenPlayerAndMouse);
+        }
+    }
+}
